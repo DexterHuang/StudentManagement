f3d23c3d34fb9485c1d6c8eaae8d58ae5dad0c33
jack2 <jack2@DESKTOP-BBFRGM8>
2016-05-15 00:51:49 +0800

    hhh

--- src/studentmanagement/StudentManagement.java
+++ src/studentmanagement/StudentManagement.java
@@ -15,7 +15,7 @@ public class StudentManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //test test 
     }
 
 }
